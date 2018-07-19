f1e987058b9944644c444afbd8df9de3cabde28a
92andrew.brady@gmail.com <92andrew.brady@gmail.com>
2018-07-19 10:18:32 -0700

    Updated addItem function to toggel strikethrough classList

--- app.js
+++ app.js
@@ -1,15 +1,20 @@
+//Create a query selector function similar to jQuery 
+const $ = (selector) => {
+    return document.querySelector(selector);
+}
+
+const item = $('#dynamic-list').getElementsByTagName('li');
+
+
 //Create a function that will add user input to list of todo items
-let addItem = () => {
-    //Store the html ul tage in a variable
-    let container = document.getElementById("list-container");
-    //Create a list item for each input
-    let li = document.createElement('li');
-    //Store the input value from user in a variable
-    let item = document.getElementById("list-value").value;
-    //Give that list item a class tage for style purposes
-    li.setAttribute('class', 'list-item');
-    //Assign the user input value to the created li tag
-    li.textContent = item;
-    //Append the list item to the ul tag and return the value
-    return container.appendChild(li);
+const addItem = () => {
+    let container = document.getElementById("dynamic-list");        //Store the html ul tage in a variable
+    let li = document.createElement('li');                          //Create a list item for each input
+    let item = document.getElementById("list-value").value;         //Store the input value from user in a variable
+    li.setAttribute('class', 'list-item');                          //Give that list item a class tage for style purposes
+    li.textContent = item;                                          //Assign the user input value to the created li tag
+    li.addEventListener('click', () => {                            // Create event listener function to toggle strikethrough class
+        li.classList.toggle('strike-item');
+    });
+    return container.appendChild(li);                               //Append the list item to the ul tag and return the value
 }
